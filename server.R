add82d8340d70602d2f4f5b421948ba45c306f16
Eric Hare <ericrhare@gmail.com>
2014-07-18 10:20:17 -0400

    Writing to the database supported

--- server.R
+++ server.R
@@ -6,12 +6,33 @@ library(RMySQL)
 # Define server logic required to draw a histogram
 shinyServer(function(input, output, session) {
     
-    observe({
-        input$submit
-        
-        con <- dbConnect(MySQL(), user="root", password="ChrtRt5rt%",
-                          dbname="mahbub", host="localhost")
-        
+    output$result <- renderText({            
+            input$submit
+            
+            if (input$submit == 0) {
+                return("")
+            }
+            
+            isolate({
+                x <- ceiling(4 * input$xcoord / input$plotwidth)
+                y <- ceiling(4 * input$ycoord / input$plotheight)
+                
+                reason <- input$reasoning
+                if (reason == "oth") reason <- paste(reason, input$other, sep = ": ")
+                
+                test <- data.frame(ip_address = "0.0.0.0", nick_name = input$turk, start_time = 0, end_time = 0, 
+                                   pic_id = 0, response_no = x + (4 * (y - 1)), conf_level = input$certain, 
+                                   choice_reason = reason, description = "turkshiny")
+                
+                con <- dbConnect(MySQL(), user="root", password="ChrtRt5rt%",
+                                 dbname="mahbub", host="localhost")
+                
+                dbWriteTable(con, "feedback", test, append = TRUE, row.names = FALSE)
+                
+                dbDisconnect(con)
+                
+                return("Submitted successfully!")
+            })
     })
     
     output$choice <- renderText({
